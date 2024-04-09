9241c432933a1cad0c200fed6d7464413778eac1
Rhys Smoker <r.smoker@gmail.com>
2024-04-09 15:52:51 -0700

    added title copy

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
-import "./App.css";
-import GameBoard from "./GameBoard"; // Adjust the path if your GameBoard.js is in a different folder
+import "./App.css"; // Assuming your styles are here
+import GameBoard from "./GameBoard"; // Adjust the import path if necessary
 
 function App() {
 	return (
-		<div className="App">
+		<div className="app-container">
+			<h1 className="app-title">Crux</h1>
+			<p className="app-instructions">
+				Welcome to Crux! The crossword game with an dash of CodeNames. Click on
+				the magnifying glass icons on word intersections to see picture clues
+				that somehow pertain to both words. Type your solutions into the
+				squares. Good luck!
+			</p>
 			<GameBoard />
 		</div>
 	);
