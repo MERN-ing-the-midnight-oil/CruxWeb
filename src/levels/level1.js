1c604cd7287d39c4a6098e2778aa9c434e38089f
Rhys Smoker <r.smoker@gmail.com>
2024-04-15 11:44:48 -0700

    new images

--- src/levels/level1.js
+++ src/levels/level1.js
@@ -1,21 +1,23 @@
 // src/levels/level1.js
 const baseUrl = process.env.REACT_APP_IMAGE_BASE_URL || ""; // Use an empty string as fallback
-console.log("Base URL:", baseUrl); // This should log an empty string during production according to my .env setting
+console.log("Base URL:", baseUrl); // This logs the base URL for debugging purposes
 
 const level1 = {
 	words: [
-		{ word: "ship", start: { x: 0, y: 0 }, direction: "across" },
-		{ word: "package", start: { x: 3, y: 0 }, direction: "down" },
+		{ word: "Butterfly", start: { x: 0, y: 0 }, direction: "across" },
+		{ word: "Butter", start: { x: 0, y: 0 }, direction: "down" },
+		{ word: "Flywheel", start: { x: 6, y: 0 }, direction: "down" },
 	],
 	intersections: [
 		{
-			// Assuming "ship" and "package" intersect at the letter "p",
-			// and the intersection is at (x: 3, y: 0) based on "ship" starting at (0,0) and "package" at (3,0).
-			position: { x: 3, y: 0 },
-			clues: [
-				baseUrl + "images/level1/clue-ship-package-1.webp",
-				baseUrl + "images/level1/clue-ship-package-2.webp",
-			],
+			// Intersection of "Butterfly" and "Butter" at (x: 0, y: 0)
+			position: { x: 0, y: 0 },
+			clues: [baseUrl + "/images/level1/clue-butterfly-butter-1.webp"],
+		},
+		{
+			// Intersection of "Butterfly" and "Flywheel" at (x: 6, y: 0)
+			position: { x: 6, y: 0 },
+			clues: [baseUrl + "/images/level1/clue-butterfly-flywheel-1.webp"],
 		},
 		// Additional intersections can be added here if your game expands
 	],
